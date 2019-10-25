bb65d80f0a94b4940e94724c94d5fb1883d64ac4
kwiss <christophe@hooraylabs.com>
2019-10-25 18:29:30 +0200

    fix(seed): add back right path

--- packages/knex/knexfile.js
+++ packages/knex/knexfile.js
@@ -33,7 +33,7 @@ module.exports = {
       directory: join(__dirname, "migrations")
     },
     seeds: {
-      directory: join(__dirname, "seeds/production")
+      directory: join(__dirname, "seeds/development")
     }
   },
   production: {
